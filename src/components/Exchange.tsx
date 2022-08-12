98e0f4ffda7295a9f875910bf8a3105a175f7b2a
Robert Kelly <robertkelly1203@gmail.com>
2022-08-12 20:00:43 +0100

    directory to separate component

--- src/components/Exchange.tsx
+++ src/components/Exchange.tsx
@@ -3,16 +3,20 @@ import { type ReactNode} from "react";
 import { Link } from "react-router-dom";
 
 interface ExchangeProps {
-    exchange: Record<string, ReactNode>;
+    exchange: Record<string, string> | Record<string, ReactNode>;
+    image: string
+    // exchange.image: Record<string, string> 
+    // probably need to write out individual info 
 }
 
-const Exchange = ({ exchange }: ExchangeProps) => {
+const Exchange = ({ exchange, image }: ExchangeProps) => {
 
   return (
     // <Link to={`/exchanges/${exchange.id}`} className="text-decoration-none my-1 coin">
     //   <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
     <li>
-        {/* <img className="coinlist-image" src={exchange.image} alt="" /> */}
+        {/* <img className="coinlist-image" src={exchange.image as HTMLImageElement} alt="" /> */}
+        <img className="coinlist-image" src={image} alt="" />
         <span>{exchange.id}</span>
 
         <span
