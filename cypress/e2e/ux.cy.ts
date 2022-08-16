689f7ebf6cb8793f9c38f159d434209845f70e6e
Robert Kelly <robertkelly1203@gmail.com>
2022-08-16 18:36:43 +0100

    added export to cypress file to troubleshoot hosting error to vercel

--- cypress/e2e/ux.cy.ts
+++ cypress/e2e/ux.cy.ts
@@ -4,8 +4,8 @@ describe('ux', () => {
     it('user view exchanges', () => {
         // main page
         // visit first exchange page
-        // cy.visit('http://localhost:3000')
-        cy.visit('/')
+        cy.visit('http://localhost:3000')
+        // cy.visit('/')
         cy.findAllByText('Cayman Islands').then(els => {
             console.log('els', els)
             cy.wrap(els[0]).click()
@@ -15,3 +15,5 @@ describe('ux', () => {
         cy.findByRole('link', {  name: /crypto scoper/i})
     })
 })
+
+export {};
