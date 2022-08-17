c91f3bc2f98e3653c765ae947f0c86fec98033cd
Robert Kelly <robertkelly1203@gmail.com>
2022-08-17 09:27:35 +0100

    removed lint errors from Directory.tsx

--- src/components/Directory.test.tsx
+++ src/components/Directory.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { exchange } from "src/fixtures/exchange.fixture";
 import ExchangeRow from "./ExchangeRow"
 
--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import React, { useEffect, useState } from "react";
-import Exchange from './ExchangeRow';
 import coingecko from '../pages/api/coingecko';
 import ExchangeRow from './ExchangeRow';
-import Link from 'next/link';
 
 const styles = {
   directoryCard: `w-full p-5 py-3 pb-0 bg-[#323546] rounded-xl text-white mr-3`,
@@ -15,7 +11,7 @@ const styles = {
 
 
 const Directory: NextPage = () => {
-
+  // change to use API layer
   const [ exchanges, setExchanges ] = useState<any[]>([]);
 
   useEffect(() => {
@@ -40,7 +36,7 @@ const Directory: NextPage = () => {
       <table className='table-auto w-full text-sm text-left text-gray-500 dark:text-gray-40'>
         <caption className="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
               Central Exchanges
-              <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Scope the list of the top 10 CEX's below and click the exchange name to read more.</p>
+              <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{"Scope the list of the top 10 CEX's below and click the exchange name to read more."}</p>
           </caption>
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
